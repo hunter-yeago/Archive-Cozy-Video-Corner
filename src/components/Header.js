5743494934e455fb0307f6b41d91bc3e2da0e7e2
Hunter Yeago <hyradar@tutamail.com>
2023-04-29 13:31:35 -0500

    started building header

--- src/components/Header.js
+++
@@ -1,5 +0,0 @@
-export function Header() {
-    return <h1>Distraction Free Video Search</h1>
-  }
-
-export default Header;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+
+import './Header.scss';
+export function Header() {
+    return (
+      <header className='header'>
+        <h1>Distraction Free Video Search</h1>
+      </header>
+    )
+  }
+
+export default Header;
