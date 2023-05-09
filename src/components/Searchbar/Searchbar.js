a3d9a108b7d0f16e197087715cf2675a0af060d7
Hunter Yeago <hyradar@tutamail.com>
2023-05-08 21:17:59 -0500

    minor adjustments to width and height

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -9,7 +9,7 @@ export function Searchbar() {
   
     let part = 'part=snippet';
     // let idpart = 'part=snippet,contentDetails,statistics,status';
-    const maxResults = 'maxResults=3';
+    const maxResults = 'maxResults=6';
     const type = 'type=video';
     const fields = 'fields=items%2Fsnippet%2Fthumbnails';
     // const id = 'id=8xORf5t7vXE';
