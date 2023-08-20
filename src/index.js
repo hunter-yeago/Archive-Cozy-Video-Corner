3a54e3521249f135125d147d103984bd641a3cfe
hyradar <hyradar@tutamail.com>
2023-08-20 15:10:39 -0500

    fixing proxy with middleware, and adding production code if statement

--- src/index.js
+++ src/index.js
@@ -12,6 +12,14 @@ root.render(
   </React.StrictMode>
 );
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+  });
+}
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
