ef66ccb2fcf7f51c63e844f8addd9d3e35cdae08
hyradar <hyradar@tutamail.com>
2023-09-04 12:02:59 -0500

    cleaned up comments, added back in paragraph text in mobile verison of info panel, added responsivity to h1 and p font size in info panel

--- src/components/InfoPanel/InfoPanel.js
+++ src/components/InfoPanel/InfoPanel.js
@@ -21,6 +21,12 @@ export function InfoPanel(props) {
       <h1>Welcome to Cozy Video Corner!</h1>
 
       <p>
+        This is a website to allow you to enjoy the comfort of online video watching without
+        all of the distracting recommended videos and other things that these sites will oftern
+        use to get you to lose track of time and watch as many as ads as possible.
+      </p>
+
+      <p>
         This has been a fun little project of mine and I'm happy with how it's turning out!
         Click the button below to start searching for some videos in this nice distraction
         free environment :)
