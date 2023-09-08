7051cb6d241d1eb9351cacd68f58ab43e45235fd
hyradar <hyradar@tutamail.com>
2023-09-07 19:17:04 -0500

    moved styling to correct components, biggest thing was both correcting the styling on the sidebar buttons as well as cleaning up the code nicely

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Header from './components/Header/Header';
 import Videodisplay from './components/VideoDisplay/Videodisplay';
 import './styles/App.scss';
 import ControlBoard from './components/ControlBoard/ControlBoard';
